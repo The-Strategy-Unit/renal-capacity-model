9eb4dd53f4b61e897bdebfcf00a5cd0ca95d688e
YiWen Hon <yiwen.hon1@nhs.net>
2025-08-12 12:06:43 +0100

    rename stage to referral

--- renal_capacity_model/helpers.py
+++ renal_capacity_model/helpers.py
@@ -12,7 +12,7 @@ def get_interarrival_times():
         dict: Dictionary containing different interarrival times for use in model
     """
     iat_dict = {}
-    for stage, stage_value in g.referral_dist.items():
+    for referral, referral_value in g.referral_dist.items():
         for age_group, age_value in g.age_dist.items():
-            iat_dict[f"{age_group}_{stage}"] = 1 / (age_value * stage_value)
+            iat_dict[f"{age_group}_{referral}"] = 1 / (age_value * referral_value)
     return iat_dict
