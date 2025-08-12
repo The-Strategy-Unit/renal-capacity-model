0d1ed6f459fd26f94c4388a39a06a898868c9ea5
YiWen Hon <yiwen.hon1@nhs.net>
2025-08-12 10:58:40 +0100

    remove unused files, move distributions into config

--- renal_capacity_model/config.py
+++ renal_capacity_model/config.py
@@ -1,7 +1,22 @@
+"""
+This module contains the configuration for running the model.
+It can later be adapted to take inputs from users
+"""
+
+
 class g:
-    n_streams = 12
     trace = True
     number_of_runs = 100
-    patient_arrival_rate = 5
     sim_duration = 365
     random_seed = 0
+
+    # distributions for calculating interarrival times
+    age_dist = {
+        1: 0.051024846,
+        2: 0.06697011,
+        3: 0.098222828,
+        4: 0.169019801,
+        5: 0.270344708,
+        6: 0.344417708,
+    }
+    referral_dist = {"early": 0.856711916, "late": 0.143288084}
