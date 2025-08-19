4cbecd2cf06099a093c86f187f37d268d252c57c
YiWen Hon <yiwen.hon1@nhs.net>
2025-08-19 11:56:33 +0100

    started work on results recording

--- renal_capacity_model/trial.py
+++ renal_capacity_model/trial.py
@@ -2,6 +2,7 @@
 Module containing Trial class with logic for running multiple model iterations
 """
 
+from turtle import mode
 import pandas as pd
 from model import Model
 from config import g
@@ -27,6 +28,17 @@ class Trial:
         for run in range(g.number_of_runs):
             model = Model(run, self.rng)
             model.run()
+            self.df_trial_results.loc[run, "diverted_to_con_care"] = model.results_df[
+                "diverted_to_con_care"
+            ].sum()
+            diverted_to_con_care = model.results_df.groupby("age_group")[
+                "diverted_to_con_care"
+            ].sum()
+            # TODO: instead of denominator being sum of diverted to con_care, it should be total number of patients of that age group which have entered the system
+            # for age_group in diverted_to_con_care.index:
+            #     self.df_trial_results.loc[run, f"diverted_to_con_care_{age_group}"] = (
+            #         diverted_to_con_care.loc[age_group] / diverted_to_con_care.sum()
+            #     )
             # We currently only save counts of patients in the system
             for k, v in model.patients_in_system.items():
                 self.df_trial_results.loc[run, k] = v
