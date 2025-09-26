fd569131f71d49cbcbf4aa14b527f745d24a632f
YiWen Hon <yiwen.hon1@nhs.net>
2025-09-26 16:22:39 +0100

    add very basic end-to-end test and test for config

---
+++ tests/end-to-end/test_end_to_end.py
@@ -0,0 +1,29 @@
+import pytest
+from renal_capacity_model.model import Model
+from renal_capacity_model.config import Config
+import numpy as np
+from renal_capacity_model.trial import Trial
+
+
+@pytest.fixture
+def config():
+    return Config()
+
+
+@pytest.fixture
+def rng():
+    return np.random.default_rng(1)
+
+
+def test_single_model_run(config, rng):
+    model = Model(1, rng, config)
+    model.run()
+    assert model.results_df.shape[0] > 0
+    assert model.results_df.shape[1] == 13
+
+
+def test_full_trial_run(config):
+    trial = Trial(config)
+    trial.run_trial()
+    assert trial.df_trial_results.shape[0] > 0
+    trial.run_trial
---
+++ tests/test_config.py
@@ -0,0 +1,18 @@
+import pytest
+from unittest.mock import Mock
+
+from renal_capacity_model.config import Config
+
+
+@pytest.mark.parametrize(
+    "config_dict, expected",
+    [({}, (False, 0)), ({"trace": True, "random_seed": 99}, (True, 99))],
+)
+def test_config_initialises_with_values(config_dict, expected):
+    # arrange
+    # act
+    config = Config(config_dict)
+
+    # assert
+    config.trace == expected[0]
+    config.random_seed == expected[1]
