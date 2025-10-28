718a2d51e202b65462d00f22264998d5292bc648
Morgan Lucy (MLCSU) <Lucy.Morgan@mlcsu.nhs.uk>
2025-10-28 09:53:09 +0000

    edit to test_entity due to new argument for patient

--- tests/unit/test_entity.py
+++ tests/unit/test_entity.py
@@ -5,12 +5,14 @@ def test_entity_initializes_with_values():
     # arrange
     p_id = 1
     patient_type = "0_referraltype"
+    patient_flag = "incident"
 
     # act
-    patient = Patient(p_id, patient_type)
+    patient = Patient(p_id, patient_type, patient_flag)
 
     # assert
     assert patient.id == p_id
     assert patient.patient_type == patient_type
+    assert patient.patient_flag == patient_flag
     assert patient.age_group == 0
     assert patient.referral_type == "referraltype"
