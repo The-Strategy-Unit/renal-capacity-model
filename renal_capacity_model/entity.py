d18bfa1e0fcbb2705e0ceaf2cf5cf91bb11c5f96
YiWen Hon <yiwen.hon1@nhs.net>
2025-10-30 16:35:52 +0000

    fix type hints in Patient entity

--- renal_capacity_model/entity.py
+++ renal_capacity_model/entity.py
@@ -6,7 +6,9 @@ Contains the Entities to be used in the model
 class Patient:
     """Patient entity"""
 
-    def __init__(self, p_id, patient_type, start_time_in_system, patient_flag):
+    def __init__(
+        self, p_id: int, patient_type: str, start_time_in_system: int, patient_flag: str
+    ):
         self.id = p_id
         self.time_in_system = 0
         self.patient_type = patient_type
@@ -15,15 +17,17 @@ class Patient:
         self.age_group = int(
             patient_type.split("_")[0]
         )  # Extract age group from patient type
-        self.referral_type = patient_type.split("_")[1]
-        self.transplant_suitable = None  # transplant_suitable
-        self.transplant_type = None  # transplant_type  # "live", "cadaver"
-        self.pre_emptive_transplant = None
-        self.dialysis_modality = None  # dialysis_modality  # none, ichd, hhd, pd
+        self.referral_type: str = patient_type.split("_")[1]
+        self.transplant_suitable: bool | None = None  # transplant_suitable
+        self.transplant_type: str | None = None  # transplant_type  # "live", "cadaver"
+        self.pre_emptive_transplant: bool | None = None
+        self.dialysis_modality: str | None = (
+            None  # dialysis_modality  # none, ichd, hhd, pd
+        )
         self.time_on_dialysis = {"ichd": 0, "hhd": 0, "pd": 0}
-        self.time_living_with_live_transplant = None
-        self.time_living_with_cadaver_transplant = None
+        self.time_living_with_live_transplant: float | None = None
+        self.time_living_with_cadaver_transplant: float | None = None
         self.transplant_count = 0  # transplant_count
-        self.time_on_waiting_list = None
-        self.time_enters_waiting_list = None
-        self.time_of_transplant = None
+        self.time_on_waiting_list = 0
+        self.time_enters_waiting_list: float | None = None
+        self.time_of_transplant: float | None = None
